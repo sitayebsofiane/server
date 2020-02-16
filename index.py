957f157af50c9e30e345551fee574641f45c69a5
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-02-16 13:32:14 +0100

    add file server python

--- index.py
+++ index.py
@@ -1,9 +1,21 @@
 #coding:utf-8
 import cgi
+import http.cookies
+import datetime
+import os
+expiration = datetime.datetime.now() + datetime.timedelta(days=365)
+expiration = expiration.strftime("%a,%d-%b-%Y %H:%M:%S")
+my_cookie = http.cookies.SimpleCookie()
+my_cookie["pref_lang"] = "fr"
+my_cookie["pref_lang"]["expires"] = expiration
+my_cookie["pref_lang"]["expires"] = True
 
+ 
+print(my_cookie.output())
 print("Content-type: text/html; charset=utf-8\n")
+
 var="str"
-html="""
+html_head = """
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,14 +26,19 @@ html="""
                 }
     </style>
     <title>Page test python html</title>
-</head>
+</head>"""
+html_body = """
 <body>
     <h1>hello tout le monde</h1>
     <h2>si voulez gerer les conference taper <a href="conferences.py">ici</a></h2>
     <h2>si voulez gerer les conferencier taper <a href="speakers.py">ici</a><h2p>
-    
+"""
+html_footer = """
 </body>
 </html>
 
 """
-print(html)
+if "HTTP_COOKIE" in os.environ:
+    print(html_head+html_body+os.environ["HTTP_COOKIE"]+html_footer)
+else:
+    print(html_head+html_body+"<h1>HTTP_COOKIE n'existe pas</h1>"+html_footer)
